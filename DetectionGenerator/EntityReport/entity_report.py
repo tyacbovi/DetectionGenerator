4c0d1a0c507f5e1b52df5d407b16b09dbbf60255
tyacbovi <tyacbovi@gmail.com>
2017-03-28 17:51:21 +0300

    data format : fix fields naming to align to reader

--- DetectionGenerator/EntityReport/entity_report.py
+++ DetectionGenerator/EntityReport/entity_report.py
@@ -2,7 +2,7 @@ from collections import namedtuple
 from simplejson import dumps
 
 
-class EntityReport(namedtuple("EntityReport", ("id", "location_lat", "location_long", "source_name", "category",
+class EntityReport(namedtuple("EntityReport", ("id", "lat", "xlong", "source_name", "category",
                                                "speed", "course", "elevation", "nationality", "picture_url", "height",
                                                "nickname"))):
     def to_json(self):
